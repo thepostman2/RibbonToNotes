a9824f5bfa70a099b9aac82ccaa606a5b622003c
PJP <synthpostman@gmail.com>
2025-04-21 11:03:18 +0200

    chore: faster checking in MidiMessageComplies

--- Source/AtomicMidiInfo.h
+++ Source/AtomicMidiInfo.h
@@ -23,14 +23,21 @@ struct AtomicMidiInfo
     bool MidiMessageComplies(const juce::MidiMessage &midiMessage)
     {
         auto messageType = midiMessage.isController() ? 1 : midiMessage.isNoteOn() ? 2 : 0;
-        if(messageType == 0) return false;
-        auto messageChannel = midiMessage.getChannel();
+        if(messageType == 0 || messageType != (int) *MessageType) return false;
+
         auto messageNumber = messageType == 1 ? midiMessage.getControllerNumber() : midiMessage.getNoteNumber();
+        if(messageNumber != (int) *Number) return false;
+
+        auto messageChannel = midiMessage.getChannel();
+        if((int) *Channel != 0 && messageChannel != (int) *Channel) return false;
+        
         auto messageValue = messageType == 1 ? midiMessage.getControllerValue() : midiMessage.getVelocity();
 
         return (messageType == (int) *MessageType
            && messageNumber == (int) *Number
-           && ((int) *Channel == 0 || messageChannel == (int) *Channel));
+           && ((int) *Channel == 0 || messageChannel == (int) *Channel)
+                && messageValue >= (int) *MinValue
+                && messageValue < (int) *MaxValue);
 
     }
 };
